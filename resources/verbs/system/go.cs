e8337460e80fe6c14e543ad39c9e346c7bdb893d
Krenath <erickoske@gmail.com>
2025-07-29 23:25:31 -0400

    dress up go command a bit

--- resources/verbs/system/go.cs
+++ resources/verbs/system/go.cs
@@ -2,72 +2,72 @@
 var currentLocation = Player.Location;
 if (currentLocation is null)
 {
-notify(Player, "<p class='error'>You are not in any location.</p>");
-return;
+    notify(Player, "<p class='error' style='color:red'>You are not in any location.</p>");
+    return;
 }
 var exits = Builtins.GetExitsFromRoom(currentLocation);
 var exitNames = new List<string>();
 foreach (var exit in exits)
 {
-var dir = Builtins.GetProperty(exit, "direction");
-if (dir != null)
-{
-exitNames.Add(dir);
+    var dir = Builtins.GetProperty(exit, "direction");
+    if (dir != null)
+    {
+        exitNames.Add(dir);
+    }
 }
-}
-var availableExits = $"Available exits: {string.Join(", ", exitNames)}</p>";
+var availableExits = $"Available exits: <span class='param' style='color:yellow'>{string.Join(", ", exitNames)}</span>";
 if (Args.Count == 0)
 {
-// Show available exits if no direction given
-if (exits.Count == 0)
-{
-notify(Player, "<p class='error'>There are no exits from here.</p>");
-}
-else
-{
-notify(Player, availableExits);
-notify(Player, "Usage: go &lt;direction&gt;");
-}
-return;
+    // Show available exits if no direction given
+    if (exits.Count == 0)
+    {
+        notify(Player, "<p class='error' style='color:red'>There are no exits from here.</p>");
+    }
+    else
+    {
+        notify(Player, availableExits);
+        notify(Player, "<p class='usage' style='color:green'>Usage: <span class='command' style='color:yellow'>go <span class='param' style='color:gray'>&lt;direction&gt;</span></span></p>");
+    }
+    return;
 }
 var chosenDirection = String.Join(" ",Args).ToLowerInvariant();
 dynamic chosenExit = null;
 foreach (var exit in exits)
 {
-var exitDirection = Builtins.GetProperty(exit, "direction")?.ToString().ToLowerInvariant();
-if (exitDirection == chosenDirection || exitDirection == $"\"{chosenDirection}\"") // for some reason, property values are coming back quoted
-{
-chosenExit = exit as dynamic;
-break;
-}
+    var exitDirection = Builtins.GetProperty(exit, "direction")?.ToString().ToLowerInvariant();
+    if (exitDirection == chosenDirection || exitDirection == $"\"{chosenDirection}\"") // for some reason, property values are coming back quoted
+    {
+        chosenExit = exit as dynamic;
+        break;
+    }
 }
 if (chosenExit == null)
 {
-// Show available exits
-if (exitNames.Count > 0)
-{
-notify(Player, $"<p class='error'>There is no exit '{chosenDirection}'. {availableExits}</p>");
-}
-else
-{
-notify(Player, "<p class='error'>There are no exits from here.</p>");
-}
-return;
+    // Show available exits
+    if (exitNames.Count > 0)
+    {
+        notify(Player, $"<p class='error' style='color:red'>There is no exit '<span class='param' style='color:yellow'>{chosenDirection}</span>'. {availableExits}</p>");
+    }
+    else
+    {
+        notify(Player, "<p class='error' style='color:red'>There are no exits from here.</p>");
+    }
+    return;
 }
 dynamic destination = chosenExit.destination;
-notify(player, destination.Name);
+
 if (destination == null)
 {
-notify(Player, "<p class='error'>That exit doesn't lead anywhere.</p>");
-return;
+    notify(Player, "<p class='error' style='color:red'>That exit doesn't lead anywhere.</p>");
+    return;
 }
 // Move the player
 if (Builtins.MoveObject(Player, destination))
 {
-notify(Player, $"<p class='success'>You go <span class='param'>{chosenDirection}</span>.</p>");
-    notify(Player, destination.longDescription);
+    notify(Player, $"<p class='success' style='color:dodgerblue'>You go <span class='param' style='color:yellow'>{chosenDirection}</span>.</p>");
+    notify(Player, destination.Description());
 }
 else
 {
-notify(Player, "<p class='error'>You can't go that way.</p>");
+    notify(Player, "<p class='error' style='color:red'>You can't go that way.</p>");
 }
